ce86970e9ed3450240d78c62b9ab1285a8279d23
digambar1411 <deshawaldigambar@gmail.com>
2024-07-19 11:52:20 +0530

    more exercise

--- JAVASCRIPT/AdvanceExercise/advncExercise.js
+++ JAVASCRIPT/AdvanceExercise/advncExercise.js
@@ -1,3 +1,5 @@
+// ques 3
+
 const btnEl = document.querySelector('.btn');
 
 const growBtn =() =>{
@@ -8,3 +10,15 @@ const growBtn =() =>{
 }
 
 btnEl.addEventListener('click',()=>setTimeout(growBtn,2000));
+
+// ques 5
+const sum=(arr)=>{
+  let sum =0;
+  arr.forEach(item => sum+=item);
+  return sum;
+}
+console.log('sum is',sum([1,2,3,4,5]));
+
+// que 6
+const getSquares =(arr) => arr.map(item => item*item);
+console.log('squares are',getSquares([1,2,3,4,5]));
