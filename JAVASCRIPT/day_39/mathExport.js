4daf93ffec2d4a70bb08bcd7bad78c7e2f0c8399
digambar1411 <deshawaldigambar@gmail.com>
2024-08-15 23:00:47 +0530

    import export

---
+++ JAVASCRIPT/day_39/mathExport.js
@@ -0,0 +1,4 @@
+// named export 
+export const add = (a,b) =>a+b
+
+export const mul = (a,b) =>a*b
---
+++ JAVASCRIPT/day_39/mathImport.js
@@ -0,0 +1,8 @@
+// import { add, mul } from "./mathExport";
+import * as sd from './mathExport';
+
+const {add, mul} = sd; 
+
+add(5,6)
+
+mul(5,6)
