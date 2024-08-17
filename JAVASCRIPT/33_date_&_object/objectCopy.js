ac48df74d70ae52911e84087b7a568f62c419941
digambar1411 <deshawaldigambar@gmail.com>
2024-08-17 21:07:24 +0530

    refactor shallow & deep copy

--- JAVASCRIPT/33_date_&_object/objectCopy.js
+++ JAVASCRIPT/33_date_&_object/objectCopy.js
@@ -2,30 +2,54 @@ const person = {
   name: "John",
   age: 30,
   address: {
-      street: "123 Main St",
       city: "New York",
       country: "USA"
   },
 };
 
+const person2 = {
+  name: "John",
+  age: 30,
+  address: {
+      city: "New York",
+      country: "USA"
+  },
+}
 
- //shallow copy
+const person3 = {
+  name: "John",
+  age: 30,
+  address: {
+      city: "New York",
+      country: "USA"
+  },
+}
+
+ //copy using reference assignment
 const copiedObject = person; 
-copiedObject.name='dig';
+copiedObject.age=12;
 
 console.log('original object',person);
-console.log('copied object using shallow copy',copiedPerson);
+console.log('copied object using reference assignment',copiedObject);
 
-// using rest 
-const copiedObjectUsingRest = {...copiedObject}
+//shallow copy using rest 
+const copiedObjectUsingRest = {...person2}
+copiedObjectUsingRest.age=12;
+copiedObjectUsingRest.address.country = 'ind';
+console.log('original object',person2);
+console.log('copied object using rest',copiedObjectUsingRest);
 
-// using object.assign
-const copyObject = Object.assign({}, person);
-
+//shallow copy using object.assign
+const copyObject = Object.assign({}, person3);
+copyObject.age=12;
+copyObject.address.country = 'PAK';
+console.log('original object',person3);
+console.log('copied object using assig',copyObject);
 
 // deep copy using JSON 
 const copiedPerson = JSON.parse(JSON.stringify(person));  //deep copy
-copiedPerson.name='mahesh';
+copiedPerson.age=82;
+copiedPerson.address.country='AUS';
 
 console.log('original object',person);
-console.log('copied object using deep copy',copiedPerson);
+console.log('deep copy using json',copiedPerson);
